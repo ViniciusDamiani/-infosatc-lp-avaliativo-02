c2a2348f0d00dbdeed0be7bec414be65771177a7
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-05 16:12:21 -0300

    feat: exercicio04

---
+++ .vscode/exercicio04.py
@@ -0,0 +1,6 @@
+#Escreva um programa para copiar uma lista em outra
+listaMercado = ["pepsi","coca","fanta"]
+print(listaMercado)
+
+listaVendinha = listaMercado.copy()
+print("O mercado foi copiado: ", listaVendinha)

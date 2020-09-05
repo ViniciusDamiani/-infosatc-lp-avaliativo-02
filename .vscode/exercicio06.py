9bf0af2a2a97edeb11f74b55d07b0cb72d959c6c
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-05 16:13:34 -0300

    feat: exercicio06

---
+++ .vscode/exercicio06.py
@@ -0,0 +1,11 @@
+#Faça 4 listas:
+filmes = ["O bicho vai pegar","Doutor Estranho","Avatar"]
+jogos = ["Pes2020", "CS", "GTA"]
+livros = ["A Filosofia do Sucesso", "O Monge e o Executivo", "Principios Ray Dalio"]
+esportes = ["Futebol", "Basquete", "Skate"]
+#a) Crie uma lista das 4 listas criadas acima.
+listas = filmes + jogos + livros + esportes
+print(listas)
+#b) Acesse(mostrar) algum item da lista livros.
+print("Recomendo: ", livros[2])
+

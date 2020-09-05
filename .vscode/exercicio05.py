de32b096c8c1d4ca484c4d199241afb6b8a9230b
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-05 16:12:56 -0300

    feat: exercicio05

---
+++ .vscode/exercicio05.py
@@ -0,0 +1,8 @@
+#Escreva um programa para verificar se um elemento x está presente em uma lista.
+listaPresenca = ["Vinicius","Damiani"]
+if "Vinicius" in listaPresenca:
+    print("Sim, ele veio hoje!")
+else:
+    print("Não, o ônibus deve ter quebrado ")
+
+    

3ede60bd86569e43b1841c3e16cb63b5d00aaf4c
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-05 16:14:57 -0300

    feat: exercicio07

---
+++ .vscode/exercicio07.py
@@ -0,0 +1,7 @@
+#Depois de ir para o carrinho de compras na loja online, você percebeu que
+meuPc=["Monitor","Gabinete Coler Master","GeForce RTX 2080Ti ","Perifericos"]
+print(meuPc)
+
+del meuPc [1]
+del meuPc [2]
+print("Faltou dinheiro para: ", meuPc)

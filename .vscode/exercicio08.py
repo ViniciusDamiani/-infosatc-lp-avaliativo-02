6ae896b31e512674fc43ea3ac687543b17a745a4
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-05 16:16:16 -0300

    feat: exercicio08

---
+++ .vscode/exercicio08.py
@@ -0,0 +1,3 @@
+#Faça uma lista de números inteiros em ordem aleatória e mostre ao usuário a lista ordenada.
+listaOrdem = ["2","4","1","8","9","3"]
+print("A lista em ordem crescente ficou: ", sorted(listaOrdem))
